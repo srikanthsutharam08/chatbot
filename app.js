1f6b5571ce73f1d9e28c48c886b377aaf6a34920
srikanth-sutharam <srikanth.sutharam@peel.com>
2016-08-03 14:29:50 +0530

    Modifed to change the name and package.json

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
- res.sendFile(__dirname + '/index.html');
-});
-io.on('connection', function(socket){
- socket.on('chat message', function(msg){
- io.emit('chat message', msg);
- });
-});
-http.listen(port, function(){
- console.log('listening on ' + port);
-});
+var builder = require('botbuilder')
+var restify = require('restify')
+
+var server = restify.createServer()
+server.listen(3978, function() {
+	console.log("listening on ", server.name, server.url)
+})
+var connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+})
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen())
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents)
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
